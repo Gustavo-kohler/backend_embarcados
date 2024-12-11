a8266fe9bafdb944c9c0537fda8748c8602d888f
Gustavo <gustavoluizkohler@gmail.com>
2024-12-11 19:28:32 -0300

    inicializado logs

--- log/log_model.py
+++ log/log_model.py
@@ -1,4 +1,6 @@
 import sqlalchemy as sa
+from pydantic import BaseModel
+from typing import Union
 
 
 class LogModel:
@@ -6,14 +8,20 @@ class LogModel:
         self.log = sa.Table(
             'log',
             metadata,
-            sa.Column('id', sa.Integer, primary_key=True),
-            sa.Column('message', sa.String(255)),
+            sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+            sa.Column('luz', sa.Integer),
+            sa.Column('umidade', sa.Integer),
             sa.Column('created_at', sa.DateTime, default=sa.func.now())
         )
 
-    def insert(self, conn, message):
-        conn.execute(self.log.insert().values(message=message))
+    def insert(self, conn, luz, umidade):
+        conn.execute(self.log.insert().values(luz=luz, umidade=umidade))
 
     def select_all(self, conn):
         return conn.execute(self.log.select()).fetchall()
 
+class BaseLog(BaseModel):
+    id: Union[int, None] = None
+    umidade: Union[int, None] = None
+    luz: Union[int, None] = None
+    created_at: Union[int, None] = None
